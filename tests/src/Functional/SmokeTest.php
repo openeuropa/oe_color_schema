e37a160e4c5263c7dfe21bdc15ed29e8d31f4a7b
drishu <aszilagyi@live.com>
2024-03-01 09:42:03 +0200

    EPHEH-314: Smoke test.

---
+++ tests/src/Functional/SmokeTest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\oe_color_schema\Functional;
+
+use Drupal\Tests\BrowserTestBase;
+
+/**
+ * Smoke test to check that CI is working.
+ */
+class SmokeTest extends BrowserTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = [
+    'oe_color_schema',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $defaultTheme = 'stark';
+
+  /**
+   * Tests that the module install correctly.
+   *
+   * To be removed when other tests are implemented.
+   */
+  public function testModuleInstallation(): void {
+    $this->drupalGet('<front>');
+    $this->assertSession()->statusCodeEquals(200);
+  }
+
+}

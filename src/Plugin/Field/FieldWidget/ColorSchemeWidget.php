69552ce8c2d9fc5233708345bef9f28f19596168
drishu <aszilagyi@live.com>
2024-03-15 13:44:55 +0200

    EPHEH-315: Add color scheme widget.

---
+++ src/Plugin/Field/FieldWidget/ColorSchemeWidget.php
@@ -0,0 +1,130 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\oe_color_scheme\Plugin\Field\FieldWidget;
+
+use Drupal\Core\Extension\ThemeExtensionList;
+use Drupal\Core\Field\FieldDefinitionInterface;
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\WidgetBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Theme\ThemeManagerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Plugin implementation of the 'Color Scheme' widget.
+ *
+ * @FieldWidget(
+ *   id = "oe_color_scheme_widget",
+ *   label = @Translation("Color Scheme widget"),
+ *   field_types = {
+ *     "oe_color_scheme"
+ *   }
+ * )
+ */
+class ColorSchemeWidget extends WidgetBase {
+
+  /**
+   * The theme manager.
+   *
+   * @var \Drupal\Core\Theme\ThemeManagerInterface
+   */
+  protected $themeManager;
+
+  /**
+   * The theme extension list.
+   *
+   * @var \Drupal\Core\Extension\ThemeExtensionList
+   */
+  protected $themeExtensionList;
+
+  /**
+   * Constructs a ColorSchemeWidget object.
+   *
+   * @param string $plugin_id
+   *   The plugin_id for the widget.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Field\FieldDefinitionInterface $field_definition
+   *   The definition of the field to which the widget is associated.
+   * @param array $settings
+   *   The widget settings.
+   * @param array $third_party_settings
+   *   Any third party settings.
+   * @param \Drupal\Core\Theme\ThemeManagerInterface $theme_manager
+   *   The theme manager.
+   * @param \Drupal\Core\Extension\ThemeExtensionList $theme_extension_list
+   *   The theme extension list.
+   */
+  public function __construct($plugin_id, $plugin_definition, FieldDefinitionInterface $field_definition, array $settings, array $third_party_settings, ThemeManagerInterface $theme_manager, ThemeExtensionList $theme_extension_list) {
+    parent::__construct($plugin_id, $plugin_definition, $field_definition, $settings, $third_party_settings);
+    $this->themeManager = $theme_manager;
+    $this->themeExtensionList = $theme_extension_list;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $plugin_id,
+      $plugin_definition,
+      $configuration['field_definition'],
+      $configuration['settings'],
+      $configuration['third_party_settings'],
+      $container->get('theme.manager'),
+      $container->get('extension.list.theme')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state) {
+    $options = $this->getColorSchemeOptions();
+
+    if (empty($options)) {
+      $element['#markup'] = $this->t("Color scheme options need to be defined in your active theme's info file.");
+
+      return $element;
+    }
+
+    $element['name'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Color scheme'),
+      '#options' => $options,
+      '#default_value' => $items[$delta]->name ?? NULL,
+      '#required' => $element['#required'],
+    ];
+
+    return $element;
+  }
+
+  /**
+   * Gets the color scheme names.
+   *
+   * @return array
+   *   The color scheme options.
+   */
+  protected function getColorSchemeOptions(): array {
+    $theme_name = $this->themeManager->getActiveTheme()->getName();
+    $theme_info = $this->themeExtensionList->getExtensionInfo($theme_name);
+
+    if (!empty($theme_info['color_scheme'])) {
+      // Parse the options to be more humanly readable.
+      $keys = array_keys($theme_info['color_scheme']);
+
+      $values = array_map(function ($key) {
+        $name = str_replace('_', ' ', $key);
+
+        return ucfirst($name);
+      }, $keys);
+
+      return array_combine($keys, $values);
+    }
+
+    return [];
+  }
+
+}
